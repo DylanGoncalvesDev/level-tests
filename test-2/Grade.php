df75c4b9f481a8773a3257dd12d211ebd88eb6c2
Dylan Goncalves <dylanphx93@gmail.com>
2026-06-02 16:19:47 +0200

    add second test enum: Grade

---
+++ test-2/Grade.php
@@ -0,0 +1,24 @@
+<?php 
+
+enum Grade {
+    case TenthKyu;
+    case NinthKyu;
+    case EighthKyu;
+    case SeventhKyu;
+    case SixthKyu;
+    case FifthKyu;
+    case FourthKyu;
+    case ThirdKyu;
+    case SecondKyu;
+    case FirstKyu;
+    case FirstDan;
+    case SecondDan;
+    case ThirdDan;
+    case FourthDan;
+    case FifthDan;
+    case SixthDan;
+    case SeventhDan;
+    case EighthDan;
+    case NinthDan;
+    case TenthDan;
+}
